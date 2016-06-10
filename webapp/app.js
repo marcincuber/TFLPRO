27b0c206ea765758995673ae4884e27a43c2c868
marcincuber <marcincuber@hotmail.com>
2016-06-10 21:55:59 +0100

    up

--- webapp/app.js
+++ webapp/app.js
@@ -2,13 +2,19 @@ angular.module('flapperNews', [])
 .controller('MainCtrl', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello world!';
+ 	$scope.posts = [
+	];
+	
+	$scope.addPost = function(){
+ 		if(!$scope.title || $scope.title === '') { return; }
+  		$scope.posts.push({
+    		title: $scope.title,
+    		link: $scope.link,
+    		upvotes: 0
+  		});
+  		$scope.title = '';
+  		$scope.link = '';
+	};
+	
 }]);
 
-$scope.posts = [
-  'post 1',
-  'post 2',
-  'post 3',
-  'post 4',
-  'post 5'
-];
