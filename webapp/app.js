1ca8ef5f97f7a7fba71f81be8e92aacf48aa0c62
marcincuber <marcincuber@hotmail.com>
2016-06-11 01:49:30 +0100

    up

--- webapp/app.js
+++ webapp/app.js
@@ -1,11 +1,25 @@
-var app = angular.module("app", ["xeditable"]);
+angular.module('webLinks', [])
+.controller('MainCtrl', [
+'$scope',
+function($scope){
+	$scope.links = ["www.google.com", "www.o2.co.uk", "www.three.co.uk"];
+    
+	$scope.addLink = function () {
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.links.indexOf($scope.addMe) == -1) {
+            $scope.links.push($scope.addMe);
+			$scope.errortext = "Thank you for your submittion";
+        } else {
+            $scope.errortext = "Link already in the list";
+        }
+    }
+    $scope.removeItem = function (x) {
+        $scope.errortext = "";    
+        $scope.links.splice(x, 1);
+    }
 
-app.run(function(editableOptions) {
-  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
-});
+	
+	
+}]);
 
-app.controller('Ctrl', function($scope) {
-  $scope.user = {
-    name: 'awesome user'
-  };  
-});
