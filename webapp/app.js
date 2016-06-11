8a4d07858613a1540e15827616ea2d18b676e3cd
marcincuber <marcincuber@hotmail.com>
2016-06-11 01:06:50 +0100

    up

--- webapp/app.js
+++ webapp/app.js
@@ -1,24 +1,11 @@
-angular.module('webLinks', [])
-.controller('MainCtrl', [
-'$scope',
-function($scope){
-	$scope.links = ["www.google.com", "www.o2.co.uk", "www.three.co.uk"];
-    
-	$scope.addLink = function () {
-        $scope.errortext = "";
-        if (!$scope.addMe) {return;}
-        if ($scope.links.indexOf($scope.addMe) == -1) {
-            $scope.links.push($scope.addMe);
-        } else {
-            $scope.errortext = "Link already in the list";
-        }
-    }
-    $scope.removeItem = function (x) {
-        $scope.errortext = "";    
-        $scope.links.splice(x, 1);
-    }
+var app = angular.module("app", ["xeditable"]);
 
-	
-	
-}]);
+app.run(function(editableOptions) {
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
 
+app.controller('Ctrl', function($scope) {
+  $scope.user = {
+    name: 'awesome user'
+  };  
+});
---
+++ webapp/first/app.js
@@ -0,0 +1,24 @@
+angular.module('webLinks', [])
+.controller('MainCtrl', [
+'$scope',
+function($scope){
+	$scope.links = ["www.google.com", "www.o2.co.uk", "www.three.co.uk"];
+    
+	$scope.addLink = function () {
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.links.indexOf($scope.addMe) == -1) {
+            $scope.links.push($scope.addMe);
+        } else {
+            $scope.errortext = "Link already in the list";
+        }
+    }
+    $scope.removeItem = function (x) {
+        $scope.errortext = "";    
+        $scope.links.splice(x, 1);
+    }
+
+	
+	
+}]);
+
