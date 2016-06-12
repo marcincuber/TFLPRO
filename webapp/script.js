a0fa82494a7d470c97d28ddb120a9c5239e694f3
marcincuber <marcincuber@hotmail.com>
2016-06-12 12:45:49 +0100

    up

--- webapp/script.js
+++ webapp/script.js
@@ -40,9 +40,8 @@ app.factory('ItemsService', ['$window', function($window) {
 }]);
 
 app.controller('MainCtrl', ['$scope', 'filterFilter', 'ItemsService', function ($scope, filterFilter, ItemsService) {
-	<!-- $scope.items = ItemsService.get($scope.items) -->
-	$scope.items = ["name 1", "name 2", "name 3", "name 4", "name 5", "name 6", "name 7", "name 8", "name 10", "custom", "custom 2"
-	];
+	$scope.items = ItemsService.get($scope.items)
+	<!-- $scope.items = ["name 1", "name 2", "name 3", "name 4", "name 5", "name 6", "name 7", "name 8", "name 10", "custom", "custom 2"]; -->
 	
 	$scope.addLink = function () {
         $scope.errortext = "";
