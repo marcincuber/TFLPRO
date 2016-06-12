f58bd01339324fdbf9a03d8dcd321e0311e592cb
marcincuber <marcincuber@hotmail.com>
2016-06-12 18:35:49 +0100

    up

--- webapp/script.js
+++ webapp/script.js
@@ -3,34 +3,28 @@ var app = angular.module('App', ['ui.bootstrap', 'ngCookies']);
 app.factory('ItemsService', ['$window', function($window) {
      var storageKey = 'items',
         _sessionStorage = $window.sessionStorage;
-
      return {
         // Returns stored items array if available or return undefined
         getItems: function() {
             var itemsStr = _sessionStorage.getItem(storageKey);
-
             if(itemsStr) {
                 return angular.fromJson(itemsStr);
             }         
-
             return ['name1', 'name2', 'name3']; // return default value when there is nothing stored in sessionStore                
         },
         // Adds the given item to the stored array and persists the array to sessionStorage
         putItem: function(item) {
             var itemsStr = _sessionStorage.getItem(storageKey),
             items = [];
-
             if(itemStr) {
                 items = angular.fromJson(itemsStr);
             }
 
             items.push(item);
-
             _sessionStorage.setItem(storageKey, angular.toJson(items));
         }
      }
 }]);
-
 
 app.filter('startFrom', function () {
 	return function (input, start) {
@@ -44,36 +38,39 @@ app.filter('startFrom', function () {
 
 app.controller('MainCtrl', ['$scope', 'filterFilter', function ($scope, filterFilter, ItemsService) {
 	
-	//$scope.items = ['name1', 'name2', 'name3'];
-	$scope.items = ItemsService.getItems();
+	$scope.items = ["http://o2.com", "http://o2.co.uk", "http://three.co.uk", "name 4", "name 5", "name 6", "name 7", "name 8", "name 10", "custom", "custom 2"]; 
+	//$scope.items = ItemsService.getItems();
 	
+	// cookies tests not working
 	$scope.WriteCookie = function () {
         ItemsService.putItem($scope.items);
     };
 	$scope.ReadCookie = function () {
-    	$window.alert(ItemsService.getItems($scope.items));
+    	$window.alert(ItemsService.getItems());
     };
 	
-	<!-- $scope.items = ["name 1", "name 2", "name 3", "name 4", "name 5", "name 6", "name 7", "name 8", "name 10", "custom", "custom 2"]; 
-	
+	// add a new link if it is not in the list
 	$scope.addLink = function () {
         $scope.errortext = "";
         if (!$scope.newItem) {return;}
         if ($scope.items.indexOf($scope.newItem) == -1) {
             $scope.items.push($scope.newItem);
-			ItemsService.put($scope.items);
-			$scope.errortext = "Thank you for your submition";
+			$scope.errortext = "Thank you for your submittion";
+			$scope.resetFilters();
+			ItemsService.putItem($scope.items); //not working cookies
         } else {
             $scope.errortext = "Link already in the list";
         }
     };
 	
+	// delete from list a new link if it is not in the list
 	$scope.removeItem = function(item) {
     	$scope.items.splice($scope.items.indexOf(item), 1);
-		$scope.resetFilters;
+		$scope.resetFilters();
 		$cookies.put('allItems', $scope.items)
   	};
 	
+	// get the value at the given index
 	$scope.getResult = function($index, x) {
     	$scope.indexResult = $scope.items[$index];
     	$scope.itemResult = x;
